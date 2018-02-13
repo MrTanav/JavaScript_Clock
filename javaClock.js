a555573e5e4e9fc391de3812e5b0dee99dca9ddf
Tanav Sharma <tanavsharma.ts95@gmail.com>
2018-02-12 21:30:27 -0500

    added date feature

--- javaClock.js
+++ javaClock.js
@@ -1,31 +1,27 @@
 function setup(){
 	createCanvas(400,400);
 	angleMode(DEGREES);
+
 }
 
 function draw(){
 	background(0);
-	
-	
+
 	translate(200,200);
-	
-	
-	
+
 	let hr = hour();
-	let min = minute();
+	let min= minute();
 	let sec = second();
-	
-	
-	
-	
+
 	rotate(-90);
 	strokeWeight(8);
 	
 	stroke(255, 100, 150);
 	noFill();
+
 	let secHand = map(sec, 0, 60, 0, 360);
 	arc(0,0,300,300,0,secHand);
-	
+
 	stroke(0, 255, 255);
 	let minHand = map(min, 0, 60, 0, 360);
 	arc(0,0,280,280,0,minHand);
@@ -42,5 +38,14 @@ function draw(){
 	strokeWeight(3);
 	text(hr+":"+min+":"+sec,-65,0);
 	
-	
+	var months = ["Jan","Feb","Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+
+	textSize(20);
+	strokeWeight(2);
+	let monthofyear = month();
+	var m = months[monthofyear];
+	let d = day();
+	let y = year();
+	text(m+d+" "+y,-50,40);
 }
+
